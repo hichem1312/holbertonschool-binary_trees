89242ff61b520cdc5389762836f7d31688a2840f
hichem <hichemmaatoug55@gmail.com>
2022-07-20 17:43:35 -0700

    d

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * binary_tree_insert_left - function that inserts a node as the left-child
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  * Return: node

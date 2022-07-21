8b0da3866a4a938e327b6bd995ec5fec8e01c222
hichem <hichemmaatoug55@gmail.com>
2022-07-20 17:47:48 -0700

    e

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
@@ -26,4 +26,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	parent->right = new_node;
 	return (new_node);
-}		
+}

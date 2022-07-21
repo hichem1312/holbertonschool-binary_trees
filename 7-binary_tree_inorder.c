2ad076cf4de140c2801429bf35b6fef7220b1104
hichem <hichemmaatoug55@gmail.com>
2022-07-20 19:12:10 -0700

    d

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node
- * Return: s
+ * binary_tree_inorder - create a binary tree node
+ * @tree: a pointer to the parent node
+ * @func: the value to put in the new node
+ * Return: n
  **/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
